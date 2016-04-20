93d42e59c85744df3b0676fa86d1178c6810bf1c
wd <wd@suboat.com>
2016-04-20 11:11:35 +0800

    note: http response

--- response/http_response.go
+++ response/http_response.go
@@ -1,13 +1,13 @@
 package goresponse
 
 type HTTPResponse struct {
-	Tag    string      `json:"tag"`
-	Result bool        `json:"success"`
-	Status int         `json:"status"`
-	Code   *Code       `json:"code"`
-	Meta   *Meta       `json:"meta"`
-	Data   interface{} `json:"data"`
-	Error  string      `json:"error"`
+	Tag    string      `json:"tag"`     // tag
+	Result bool        `json:"success"` // 请求是否成功处理
+	Status int         `json:"status"`  // 状态码
+	Code   *Code       `json:"code"`    // 执行码
+	Meta   *Meta       `json:"meta"`    // 扩展元
+	Data   interface{} `json:"data"`    // 核心数据
+	Error  string      `json:"error"`   // 错误描述
 }
 
 func NewHTTPResponse() *HTTPResponse {
@@ -21,12 +21,14 @@ func NewHTTPResponse() *HTTPResponse {
 	}
 }
 
+// 设置状态码
 func (r *HTTPResponse) S(s int) *HTTPResponse {
 	r.Status = s
 	r.Result = r.IsSuccess()
 	return r
 }
 
+// 设置扩展元
 func (r *HTTPResponse) ME(m *Meta) *HTTPResponse {
 	if m != nil {
 		r.Meta = m
@@ -34,16 +36,19 @@ func (r *HTTPResponse) ME(m *Meta) *HTTPResponse {
 	return r
 }
 
+// 设置执行码
 func (r *HTTPResponse) C(c int) *HTTPResponse {
 	r.Code.C(c)
 	return r
 }
 
+// 设置执行描述
 func (r *HTTPResponse) M(m int) *HTTPResponse {
 	r.Code.M(m)
 	return r
 }
 
+// 设置核心数据
 func (r *HTTPResponse) D(d interface{}) *HTTPResponse {
 	if d != nil {
 		r.Data = d
@@ -51,6 +56,7 @@ func (r *HTTPResponse) D(d interface{}) *HTTPResponse {
 	return r
 }
 
+// 设置错误描述
 func (r *HTTPResponse) E(err error) *HTTPResponse {
 	if err != nil {
 		r.Error = err.Error()
@@ -58,6 +64,7 @@ func (r *HTTPResponse) E(err error) *HTTPResponse {
 	return r
 }
 
+// 设置执行成功
 func (r *HTTPResponse) Success() *HTTPResponse {
 	r.Status = Status_success
 	r.Result = true
