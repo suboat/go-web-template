8de8af4704b2fa64812bef03c52fd453e5d94547
wd <wd@suboat.com>
2016-04-20 10:20:57 +0800

    feat: token

---
+++ token/token.go
@@ -0,0 +1,17 @@
+package gotoken
+
+type IToken interface {
+	Valid() bool
+}
+
+type Token struct {
+	Tag    string
+	Id     string
+	Level  int64
+	Status int64
+	Data   interface{}
+}
+
+func (t *Token) Valid() bool {
+	return len(t.Id) != 0
+}
