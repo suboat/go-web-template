c6dd07b2308603ba35db792f0836efa6f02f3d67
wd <wd@suboat.com>
2016-04-20 09:54:27 +0800

    hint: websocket response

--- response/websocket_response.go
+++ response/websocket_response.go
@@ -6,6 +6,6 @@ type WSResponse struct {
 	ResponseWriter http.ResponseWriter `json:"-"`
 	RequestId      string              `json:"id"`    // 请求ID,直接回复
 	URL            string              `json:"url"`   // 请求URL
-	Data           interface{}         `json:"data"`  // 回复核心数据
+	Data           interface{}         `json:"data"`  // 回复核心数据，推荐HTTPResponse
 	Error          string              `json:"error"` // 错误码
 }
