1cd7ce6d1a424d38b1e21f1e4891e5ce07577308
wd <wd@suboat.com>
2016-04-20 10:18:25 +0800

    fix: code

--- response/code.go
+++ response/code.go
@@ -4,3 +4,17 @@ type Code struct {
 	Code    int `json:"code"`
 	Message int `json:"message"`
 }
+
+func NewCode() *Code {
+	return new(Code)
+}
+
+func (c *Code) C(code int) *Code {
+	c.Code = code
+	return c
+}
+
+func (c *Code) M(message int) *Code {
+	c.Message = message
+	return c
+}
